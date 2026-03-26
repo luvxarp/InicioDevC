ed2ab336e734391b0b5993dbc44c26b50a0e1a15
Eduardo <eduardoengdev@gmail.com>
2026-03-26 18:05:20 -0300

    Teste commit pelo Terminal do Git (Adicionei um comentário no Olá Mundo)

--- ola_mundo.c
+++ ola_mundo.c
@@ -3,6 +3,7 @@
 int main(){
     
     printf("Olá, Mundo!\n");
+    //esse comentário é só um teste do git commit pelo bash, ignora, se tiver aí é pq deu certo e eu aprendi a usar git e to editando esse arquivo pelo terminal kkkk
 
     return 0;
 }
