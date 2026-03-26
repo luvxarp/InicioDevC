ec09bea892d4b8e7f3bfb05033e2ac555cdf6fb2
Eduardo <eduardoengdev@gmail.com>
2026-03-26 18:13:38 -0300

    Novo hello world de teste, vindo do Git bash, no terminal do WSL... se estiver disponível aí, deu tudo certo.

---
+++ hello_world2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello World, this is coming from Git bash, in a WSL terminal.\n
+            It's just a test, if this file is available on GitHub, everything has run ok.")
+
+}
